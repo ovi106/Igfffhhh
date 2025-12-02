cf67fc5019da624ab7d506ced36b21a88cbbe6f2
ovimim361-stack <ovimim361@gmail.com>
2025-12-02 10:35:02 +0600

    Update Emojivoice.js

--- Script/commands/Emojivoice.js
+++ Script/commands/Emojivoice.js
@@ -14,20 +14,18 @@ const fs = require("fs");
 const path = require("path");
 
 const emojiAudioMap = {
- "🥱": "https://files.catbox.moe/2r1p7g.mp3",
+ "🥱": "https://files.catbox.moe/9pou40.mp3",
  "😁": "https://files.catbox.moe/60cwcg.mp3",
  "😌": "https://files.catbox.moe/epqwbx.mp3",
  "🥺": "https://files.catbox.moe/wc17iq.mp3",
  "🤭": "https://files.catbox.moe/cu0mpy.mp3",
- "😅": "https://files.catbox.moe/aa8i16.mp3",
+ "😅": "https://files.catbox.moe/jl3pzb.mp3",
  "😏": "https://files.catbox.moe/z9e52r.mp3",
  "😞": "https://files.catbox.moe/tdimtx.mp3",
  "🤫": "https://files.catbox.moe/0uii99.mp3",
- "🐔": "https://files.catbox.moe/0vwpq1.mp3",
  "🍼": "https://files.catbox.moe/p6ht91.mp3",
  "🤔": "https://files.catbox.moe/hy6m6w.mp3",
- "🤐": "https://files.catbox.moe/sd21t9.mp3",
- "🥰": "https://files.catbox.moe/2ykkbw.mp3",
+ "🥰": "https://files.catbox.moe/dv9why.mp3",
  "🤦": "https://files.catbox.moe/ivlvoq.mp3",
  "😘": "https://files.catbox.moe/sbws0w.mp3",
  "😑": "https://files.catbox.moe/p78xfw.mp3",
@@ -36,68 +34,3 @@ const emojiAudioMap = {
  "🤨": "https://files.catbox.moe/4aci0r.mp3",
  "😡": "https://files.catbox.moe/shxwj1.mp3",
  "🙈": "https://files.catbox.moe/3qc90y.mp3",
- "😴": "https://files.catbox.moe/fr65co.mp3",
- "🤩": "https://files.catbox.moe/js834n.mp3",
- "😒": "https://files.catbox.moe/xbrjj4.mp3",
- "😍": "https://files.catbox.moe/qjfk1b.mp3",
- "😭": "https://files.catbox.moe/itm4g0.mp3",
- "😱": "https://files.catbox.moe/mu0kka.mp3",
- "😻": "https://files.catbox.moe/y8ul2j.mp3",
- "🙌": "https://files.catbox.moe/675vky.mp3",
- "😿": "https://files.catbox.moe/tqxemm.mp3",
- "💔": "https://files.catbox.moe/6yanv3.mp3",
- "🤣": "https://files.catbox.moe/2sweut.mp3",
- "🥹": "https://files.catbox.moe/jf85xe.mp3",
- "😩": "https://files.catbox.moe/b4m5aj.mp3",
- "🫣": "https://files.catbox.moe/oksyfc.mp3",
- "🤧": "https://files.catbox.moe/pfe8n4.mp3",
- "😮": "https://files.catbox.moe/n1murs.mp3",
- "😚": "https://files.catbox.moe/uhdwh3.mp3",
- "🐸": "https://files.catbox.moe/utl83s.mp3"
-};
-
-module.exports.handleEvent = async ({ api, event }) => {
- const { threadID, messageID, body } = event;
- if (!body || body.length > 2) return;
-
- const emoji = body.trim();
- const audioUrl = emojiAudioMap[emoji];
- if (!audioUrl) return;
-
- const cacheDir = path.join(__dirname, 'cache');
- if (!fs.existsSync(cacheDir)) fs.mkdirSync(cacheDir);
-
- const filePath = path.join(cacheDir, `${encodeURIComponent(emoji)}.mp3`);
-
- try {
- const response = await axios({
- method: 'GET',
- url: audioUrl,
- responseType: 'stream'
- });
-
- const writer = fs.createWriteStream(filePath);
- response.data.pipe(writer);
-
- writer.on('finish', () => {
- api.sendMessage({
- attachment: fs.createReadStream(filePath)
- }, threadID, () => {
- fs.unlink(filePath, (err) => {
- if (err) console.error("Error deleting file:", err);
- });
- }, messageID);
- });
-
- writer.on('error', (err) => {
- console.error("Error writing file:", err);
- api.sendMessage("ইমুজি দিয়ে লাভ নাই\nযাও মুড়ি খাও জান😘", threadID, messageID);
- });
-
- } catch (error) {
- console.error("Error downloading audio:", error);
- api.sendMessage("ইমুজি দিয়ে লাভ নাই\nযাও মুড়ি খাও জান😘", threadID, messageID);
- }
-};
-
-module.exports.run = () => {};
